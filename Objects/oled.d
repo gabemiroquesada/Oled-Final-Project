1f4d12632ee6af9e08fe1464d1bb306ebf2225a4
gabemiroquesada <gabemiroquesada@gmail.com>
2025-07-18 15:22:24 -0500

    Add files via upload

---
+++ Objects/oled.d
@@ -0,0 +1,3 @@
+./objects/oled.o: OLED.c OLED.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h I2C.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\string.h
---
+++ Objects/pongcourt.d
@@ -0,0 +1,2 @@
+./objects/pongcourt.o: pongcourt.c pongcourt.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h OLED.h
